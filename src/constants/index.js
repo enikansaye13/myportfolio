6c3a5009a52e93fe15c398284319fca09bd900a5
enikansaye13 <enikansaye13@gmail.com>
2024-08-12 14:09:06 +0100

    push constants file first

---
+++ src/constants/index.js
@@ -0,0 +1,223 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    angular,
+    astonlaw,
+    car,
+    contact,
+    css,
+    devcenter,
+    essential,
+    estate,
+    express,
+    git,
+    github,
+    GNT,
+    html,
+    javascript,
+    linkedin,
+    lytical,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pennyworth,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    wordpress
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: express,
+    //     name: "Express",
+    //     type: "Backend",
+    // },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: angular,
+        name: "Angular",
+        type: "Frontend",
+    },
+    {
+        imageUrl: wordpress,
+        name: "Wordpress",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: nextjs,
+    //     name: "Next.js",
+    //     type: "Frontend",
+    // },
+    // {
+    //     imageUrl: nodejs,
+    //     name: "Node.js",
+    //     type: "Backend",
+    // },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Devcenter",
+        icon: '',
+        iconBg: "#accbe1",
+        date: "October 2023 - December 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    
+    {
+        title: "Angular Developer",
+        company_name: "Lytical Technology",
+        icon: lytical,
+        iconBg: "#b7e4c7",
+        date: "Dec 2020 - Nov 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Website Manager",
+        company_name: "Sunnybest",
+        icon: '',
+        iconBg: "#a2d2ff",
+        date: "Jan 2024 - Present",
+        points: [
+            "Content Management: Updated product listings regularly, ensuring accuracy and alignment with branding.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Performance Monitoring: Analyzed site metrics and made adjustments to improve traffic and conversions.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/YourGitHubUsername',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: essential,
+        theme: 'btn-back-blue',
+        name: 'Essential Artisans',
+        description: 'I developed a web application in collaboration with the backend team. This platform is designed for artisans, offering a space to connect with skilled craftsmen and get tasks done. It includes user, artisan, and admin accounts, with the admin account overseeing all activities on the platform.',
+        link: 'https://essentialartisans.ng/',
+    },
+    {
+        iconUrl: pennyworth,
+        theme: ' btn-back-red ',
+        name: 'pennyworth saas',
+        description: 'I developed a web application in collaboration with the backend team. This platform is part of a software-as-a-service (SaaS) project specifically designed for facility companies. It provides a comprehensive solution for managing estate data and activities.',
+        link: 'https://www.pennyworthsaas.com.kwodraent.com/',
+    },
+    {
+        iconUrl: lytical,
+        theme: 'btn-back-green',
+        name: 'Lytical Technology',
+        description: 'The Lytical project is a dedicated website designed to efficiently manage and provide various services.',
+        link: 'https://lyticaltechnology.com/',
+    },
+    {
+        iconUrl: astonlaw,
+        theme: 'btn-back-green',
+        name: 'Aston Law',
+        description: 'I developed Aston Law official website, a website specifically designed for a law firm to efficiently manage and deliver their legal services',
+        link: 'https://www.astonlpng.com/',
+    },
+    {
+        iconUrl: GNT,
+        theme: 'btn-back-black',
+        name: 'GNT Nation',
+        description: 'I developed the official site for GNT Nation, where we share messages through GNT Music, blending contemporary and traditional styles to focus on themes of redemption, grace, and the power of faith. Complementing this, our charity arm, Mercy is Blessed Foundation, organizes outreach programs and events to create a tangible, positive impact on society.',
+        link: 'https://www.gntnation.com/',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Ajinde Builders',
+        description: 'Our approach to architecture is deeply rooted in the belief that every project has its unique story waiting to be told. We are committed to crafting spaces that are not just functional but also resonate with the emotions and aspirations of the people who inhabit them.',
+        link: 'https://elegant-belekoy-41f645.netlify.app/',
+    }
+];
